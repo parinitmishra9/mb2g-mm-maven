ad88ef6473b53f7a6ed9775f984ffb3208722188
parinitm <webgility123>
2020-04-24 23:25:03 +0530

    02-from-intellij

---
+++ from-intellij/src/main/java/guru/framework/intellij/HelloFromJetbrains.java
@@ -0,0 +1,13 @@
+package guru.framework.intellij;
+
+/**
+ * @Project mb2g-mm-maven
+ * Created by @Author Parinit on 4/24/2020
+ * at 11:12 PM
+ */
+public class HelloFromJetbrains {
+
+    public String sayHello() {
+        return "Hello from JetBrains!";
+    }
+}
